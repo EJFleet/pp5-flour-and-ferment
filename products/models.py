53bb08946be1256f297e845a37da19ba78170fb6
emmajf <emmafleetwood@gmail.com>
2025-02-07 20:34:42 +0000

    Add product and category fixtures, update model options, and modify template for improved navigation

--- products/models.py
+++ products/models.py
@@ -2,6 +2,10 @@ from django.db import models
 
 
 class ProductCategory(models.Model):
+
+    class Meta:
+        verbose_name_plural = 'Product categories'
+
     name = models.CharField(max_length=254)
     friendly_name = models.CharField(max_length=254, null=True, blank=True)
 
@@ -18,7 +22,6 @@ class Product(models.Model):
     name = models.CharField(max_length=254)
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
-    date_created = models.DateTimeField(auto_now_add=True)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
 
