c036ad3e32030c80d2fe3d7dd5ad7287065450cb
emmajf <emmafleetwood@gmail.com>
2025-02-08 15:30:31 +0000

    Add product detail view, template and update URLs

--- products/views.py
+++ products/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Product
 
 # Create your views here.
@@ -15,3 +15,19 @@ def all_products(request):
     }
 
     return render(request, 'products/products.html', context)
+
+
+def product_detail(request, product_id):
+
+    """ A view to show individual product details """
+
+    product = get_object_or_404(Product, pk=product_id)
+    random_products = Product.objects.exclude(id=product.id).order_by('?')[:3]
+
+
+    context = {
+        'product': product,
+        'random_products': random_products,
+    }
+
+    return render(request, 'products/product_detail.html', context)
