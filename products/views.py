8e0eb99834d24c5a0d87afb6ea1b00d8077f5675
emmajf <emmafleetwood@gmail.com>
2025-02-08 19:02:17 +0000

    Add sorting functionality to product listing and enhance category badge styles

--- products/views.py
+++ products/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
+from django.db.models.functions import Lower
+
 from .models import Product, ProductCategory
 
 # Create your views here.
@@ -14,13 +16,28 @@ def all_products(request):
     query = None
     category_names = None
     categories = None
+    sort = None
+    direction = None
 
     if request.GET:
+        if 'sort' in request.GET:
+            sort = request.GET['sort']
+            sortkey = 'lower_name' if sort == 'name' else 'category__name' if sort == 'category' else sort
+
+            if sort == 'name':
+                products = products.annotate(lower_name=Lower('name'))
+
+            if 'direction' in request.GET and request.GET['direction'] == 'desc':
+                sortkey = f'-{sortkey}'
+
+            products = products.order_by(sortkey)
 
         if 'category' in request.GET:
             category_names = request.GET['category'].split(',')
             products = products.filter(category__name__in=category_names)
             categories = ProductCategory.objects.filter(name__in=category_names)
+        else:
+            categories = ProductCategory.objects.all()
 
         if 'q' in request.GET:
             query = request.GET['q']
@@ -30,11 +47,17 @@ def all_products(request):
             
             queries = Q(name__icontains=query) | Q(description__icontains=query)
             products = products.filter(queries)
+        
+    else:
+        categories = ProductCategory.objects.all()    
+
+    current_sorting = f'{sort}_{direction}' if sort else 'name_asc'
 
     context = {
         'products': products,
         'search_term': query,
         'current_categories': categories,
+        'current_sorting': current_sorting,
     }
 
     return render(request, 'products/products.html', context)
