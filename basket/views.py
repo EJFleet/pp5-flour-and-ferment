ca13c6aa5528be2d5c19f816ebdaf984bef64461
emmajf <emmafleetwood@gmail.com>
2025-02-11 19:58:18 +0000

    Add add_to_basket view and URL for adding products to the shopping basket

--- basket/views.py
+++ basket/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.shortcuts import render, redirect, get_object_or_404
+from products.models import Product
 
 # Create your views here.
 
@@ -8,3 +10,24 @@ def view_basket(request):
     """ A view to return the basket contents page """
 
     return render(request, 'basket/basket.html')
+
+
+def add_to_basket(request, item_id):
+    """ Add a quantity of the specified product to the shopping basket """
+
+    product = get_object_or_404(Product, pk=item_id)
+    quantity = int(request.POST.get('quantity'))
+    redirect_url = request.POST.get('redirect_url')
+    basket = request.session.get('basket', {})
+
+    if item_id in list(basket.keys()):
+        basket[item_id] += quantity
+        messages.success(request, f'Updated {product.name} quantity to {basket[item_id]}')
+    else:
+        basket[item_id] = quantity
+        messages.success(request, f'Added {product.name} to your basket')
+
+    request.session['basket'] = basket
+    print(request.session['basket'])
+    return redirect(redirect_url)
+    
