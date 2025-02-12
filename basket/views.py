fb9aebf4a0172d872e048d4b6e4fe259f30e27f6
emmajf <emmafleetwood@gmail.com>
2025-02-12 19:48:47 +0000

    Add adjust and remove functionality to the basket; update URL patterns and views

--- basket/views.py
+++ basket/views.py
@@ -1,5 +1,6 @@
 from django.contrib import messages
-from django.shortcuts import render, redirect, get_object_or_404
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.http import HttpResponse
 from products.models import Product
 
 # Create your views here.
@@ -29,4 +30,39 @@ def add_to_basket(request, item_id):
 
     request.session['basket'] = basket
     return redirect(redirect_url)
-    
+
+
+def adjust_basket(request, item_id):
+    """Adjust the quantity of the specified product to the specified amount"""
+
+    product = get_object_or_404(Product, pk=item_id)
+    quantity = int(request.POST.get('quantity'))
+    basket = request.session.get('basket', {})
+
+    if quantity > 0:
+        basket[item_id] = quantity
+        messages.success(request, f'Updated {product.name} quantity to {basket[item_id]}')
+    else:
+        basket.pop(item_id)
+        messages.success(request, f'Removed {product.name} from your basket')
+
+    request.session['basket'] = basket
+    return redirect(reverse('view_basket'))
+
+
+def remove_from_basket(request, item_id):
+    """Remove the item from the shopping basket"""
+
+    try:
+        product = get_object_or_404(Product, pk=item_id)
+        basket = request.session.get('basket', {})
+
+        basket.pop(item_id)
+        messages.success(request, f'Removed {product.name} from your basket')
+
+        request.session['basket'] = basket
+        return HttpResponse(status=200)
+
+    except Exception as e:
+        messages.error(request, f'Error removing item: {e}')
+        return HttpResponse(status=500)
