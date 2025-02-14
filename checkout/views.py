9dc87f790088574c5cdd007cb7fb0f9d21a30f2c
emmajf <emmafleetwood@gmail.com>
2025-02-14 20:46:05 +0000

    Add checkout functionality with order form, views, and templates

--- checkout/views.py
+++ checkout/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
+from django.contrib import messages
+from django.conf import settings
 
-# Create your views here.
+from .forms import OrderForm
+
+
+def checkout(request):
+
+    if request.method == 'POST':
+        basket = request.session.get('basket', {})
+    
+    else:
+        basket = request.session.get('basket', {})
+        if not basket:
+            messages.error(request, "There's nothing in your basket at the moment")
+            return redirect(reverse('products'))
+        
+        order_form = OrderForm()
+        template = 'checkout/checkout.html'
+        context = {
+            'order_form': order_form,
+           }
+        
+        return render(request, template, context)
+
