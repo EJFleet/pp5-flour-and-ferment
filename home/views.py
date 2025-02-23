628a039f8bd6f35d99a0626e0175669042d07194
emmajf <emmafleetwood@gmail.com>
2025-02-23 16:14:44 +0000

    Add About page and update navigation links

--- home/views.py
+++ home/views.py
@@ -8,3 +8,10 @@ def index(request):
     """ A view to return the index page """
 
     return render(request, 'home/index.html')
+
+
+def about(request):
+
+    """ A view to return the About page """
+
+    return render(request, 'home/about.html')
