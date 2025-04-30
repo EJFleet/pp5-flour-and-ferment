95e35aab48bca9638051a3aa53a5400020be84a0
emmajf <emmafleetwood@gmail.com>
2025-04-30 20:55:16 +0100

    Bug fix: correct format of  auth_password_validators in settings.py to fix 500 error on signup page

--- config/settings.py
+++ config/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     '.devtunnels.ms',
@@ -147,31 +147,24 @@ else:
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
-        {
-            'NAME': (
-                'django.contrib.auth.password_validation.'
+    {
+        'NAME': 'django.contrib.auth.password_validation.'
                 'UserAttributeSimilarityValidator'
-            ),
-        },
-        {
-            'NAME': (
-                'django.contrib.auth.password_validation.'
-                'MinimumLengthValidator',
-            ),
-        },
-        {
-            'NAME': (
-                'django.contrib.auth.password_validation.'
-                'CommonPasswordValidator',
-            ),
-        },
-        {
-            'NAME': (
-                'django.contrib.auth.password_validation.'
-                'NumericPasswordValidator',
-            ),
-        },
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.'
+                'MinimumLengthValidator'
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.'
+                'CommonPasswordValidator'
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.'
+                'NumericPasswordValidator'
+    },
 ]
+
 
 
 # Internationalization
